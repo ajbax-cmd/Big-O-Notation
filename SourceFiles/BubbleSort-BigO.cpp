e8c7aaf99754040b59ec6f065201f09b7cac0e98
Alan <jabaxley07@gmail.com>
2021-11-13 22:36:54 -0500

    First commit

---
+++ SourceFiles/BubbleSort-BigO.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <stdlib.h>     
+#include <ctime>
+#include <stdio.h> 
+using namespace std;
+
+//This simple program tests the number of operations a bubble performs for N number of items. Change the value of N to see how the number of operations changes
+
+void swap (int &a, int &b);
+
+int main()
+{
+	const int N = 10;
+	srand (time(0));
+	int random;
+	int array[N];
+		
+	for(int i = 0; i < N; i++)
+	{
+		int random = rand() % 100;
+		array[i] = random;
+	}
+		
+	for(int i = 0; i < N; i++)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl << endl;
+		
+		
+	int innerCount = 0;	
+	int outerCount = 0;
+	int total = 0;
+	int ifStatement = 0;
+	int worstCaseIf = 0;
+	int worstCaseTotal = 0;
+	for(int i = N -1; i > 0; i--)
+	{
+		for(int j = 0; j < i; j++)
+		{
+			if(array[j] > array[j+1])
+			{
+				swap(array[j], array[j+1]);
+				ifStatement += 4;
+			}
+			innerCount++;
+			worstCaseIf += 4;
+		}
+		outerCount++;
+	}
+	total = innerCount + outerCount + ifStatement;
+	worstCaseTotal = innerCount + outerCount + worstCaseIf;
+	
+	for(int i = 0; i < N; i++)
+	{
+		cout << array[i] << " ";
+	}
+	
+	cout << "\n\ninner: " << innerCount << "\nouter: " << outerCount << "\nworst case total: " << worstCaseTotal  <<   "\nactual total: " << total;
+}
+void swap (int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;	
+}
